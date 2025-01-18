7574a54c45e258cc34ab25f9ec76981583cef0c4
Alexey Dmitrevich <oleksiidmytrevych@gmail.com>
2025-01-18 11:37:36 +0200

    Созданна таблица с корзиной и связи с таблицей user и product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,6 +12,7 @@ class ProductController extends Controller {
     public function __construct(ProductService $productService) {
         $this->productService = $productService;
     }
+
     public function search(Request $request, $product, $orderBy) {
         return $this->productService->search($request ,$product, $orderBy);
     }
