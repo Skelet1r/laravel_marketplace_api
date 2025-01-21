0c7813f4184a09c026c528b6f2215ca073a78dbc
Alexey Dmitrevich <oleksiidmytrevych@gmail.com>
2025-01-21 07:01:28 +0200

    Убран docker

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('/createAccount','createAccount')->middleware('guest')->name('createAccount');
-    Route::post('/signIn','signIn')->middleware('guest')->name('signin');
+    Route::post('/signIn','signIn')->middleware('guest')->name('signIn');
     Route::post('/logout', 'logout')->middleware('auth')->name('logout');
     Route::post('/forgotPassword','forgotPassword')->middleware('guest')->name('forgotPassword');
     Route::post('/resetPassword','resetPassword')->middleware('guest')->name('password.reset');
